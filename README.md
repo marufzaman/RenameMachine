1077bb50f28a3f072ad2262df740c931ded16657
A. M. Almarufuzzaman <marufzaman05@gmail.com>
2023-07-09 06:42:48 +0600

    Version-2

--- README.md
+++ README.md
@@ -1,7 +1,145 @@
-# RenameMachine
+# Rename Machine
 
-It is a simple file renaming script built with Python3 and Works on Windows. 
-It renames a zipped collection of multiple .srt type files at once for the folder full of video files and matches the names of all of the video file names in the directory.
-I wrote this to organize my TV Series collection.
+<hr>
 
-If anyone has any suggestions, please point those out.
+![GitHub license](https://img.shields.io/github/license/marufzaman/RenameMachine?style=flat-square)
+
+Rename Machine is a Python script that allows you to rename subtitle files in bulk based on the corresponding video
+files in a directory.
+
+## Project Description
+
+The Subtitle Renamer script simplifies the process of renaming subtitle files to match the video files they belong to.
+It extracts episode information from the video and subtitle file names and uses it to rename the subtitle files
+accordingly. This makes it easier to organize and manage subtitle files for your video collection.
+
+## Features
+
+- Rename subtitle files in bulk based on video files.
+- Extract episode information from video and subtitle file names.
+- Intelligent matching of video and subtitle files.
+- Interactive prompt for replacing existing subtitle files.
+- Cross-platform compatibility (Windows, macOS, Linux).
+
+## Future Plans
+
+- Improved error handling and error messages.
+- Support for additional video and subtitle file formats.
+- Batch processing of multiple video directories and subtitle zip files.
+- Integration with subtitle databases for automatic matching.
+- Graphical user interface (GUI) for a more user-friendly experience.
+
+## Requirements
+
+- Python 3.7 or higher
+- pydantic
+
+## Getting Started
+
+To get started with the Rename Machine, make sure you have Python 3.7 or later installed on your system. You can check
+your Python version by running the following command on your terminal or command prompt:
+
+- ##### On a Windows-based system:
+
+```shell
+python --version
+```
+
+- ##### On a Linux or macOS-based system:
+
+```shell
+python3 --version
+```
+
+If you have an older Python version installed, it is recommended to upgrade to Python 3.7.x or later for compatibility
+with the Rename Machine.
+
+### Installation and Usage
+
+**1.** Clone the repository:
+
+```shell
+git clone https://github.com/marufzaman/RenameMachine.git
+```
+
+> **Note:** Please after cloning the repository, go inside the `RenameMachine/` directory.
+
+```shell
+cd ./RenameMachine/
+```
+
+> The `RenameMachine/.` directory tree should look like as the following:
+
+```
+RenameMachine/.
+├── LICENSE
+├── README.md
+├── Rename_Machine.py
+└── requirements.txt
+```
+
+**2.** Create and activate a virtual environment inside the project directory (optional but recommended):
+
+- ##### On a Windows-based system:
+
+```shell
+python -m venv venv && venv\Scripts\activate
+```
+
+- ##### On a Linux or macOS-based system:
+
+```shell
+python3 -m venv venv && source venv/bin/activate
+```
+
+**3.** Install the dependencies using pip inside the project directory and make sure you are using a virtual
+environment:
+
+- ##### On a Windows-based system:
+
+```shell
+pip install --no-cache-dir -r requirements.txt
+```
+
+- ##### On a Linux or macOS-based system:
+
+```shell
+pip3 install --no-cache-dir -r requirements.txt
+```
+> **Note:** Here, `--no-cache-dir` is used to avoid cache dir for the pip installation.
+
+**4.** Run the script:
+
+- ##### On a Windows-based system:
+
+```shell
+python Rename_Machine.py
+```
+
+- ##### On a Linux or macOS-based system:
+
+```shell
+python3 Rename_Machine.py
+```
+
+**5.** Follow the prompts to enter the video directory and subtitle zip file paths.
+
+**6.** The script will extract the subtitle files from the zip file and attempt to rename them based on the corresponding video files in the specified directory.
+
+**7.** If a subtitle file already exists in the video directory with the same name, the script will ask whether to replace it. Enter 'Y' for Yes or 'N' for No.
+
+**8.** The script will rename the subtitle files and display a summary of the results.
+
+**9.** If no video files matched with any subtitle files, an error message will be shown.
+
+**10.** You can choose to rename again by entering 'Y' when prompted. To exit the script, enter 'N'.
+
+---
+
+## Contributing
+
+Contributions are welcome! If you have any ideas, improvements, or bug fixes, please submit a pull request. For major changes, please open an issue first to discuss the proposed changes.
+
+## Licence
+
+This project is licenced under the [MIT Licence](LICENSE).
--- Rename_Machine.py
+++ Rename_Machine.py
@@ -1,9 +1,8 @@
 import os
 import shutil
 import zipfile
-
+import re
 from pydantic import BaseModel, ValidationError, FilePath, DirectoryPath
-
 
 class SubtitleRenameInput(BaseModel):
     video_directory: DirectoryPath
@@ -15,7 +14,6 @@ class SubtitleRenameInput(BaseModel):
             try:
                 video_directory = input('Enter the Video Directory: ')
                 subtitle_zip_file = input('Enter the Subtitle Zip File: ')
-
                 return cls(
                     video_directory=os.path.abspath(video_directory),
                     subtitle_zip_file=os.path.abspath(subtitle_zip_file),
@@ -23,43 +21,94 @@ class SubtitleRenameInput(BaseModel):
             except ValidationError as e:
                 print(f"Input validation error: {e.errors()}")
 
+def extract_episode_number(file_name):
+    if match := re.search(r'[sS](\d+)[eE](\d+)', file_name):
+        return match[1], match[2]
+    return None
 
-def rename_machine(input_data):
+def rename_machine(input_data):  # sourcery skip: low-code-quality
     video_path = input_data.video_directory
     subtitle_zip_file = input_data.subtitle_zip_file
 
-    video_files = [
-        os.path.splitext(file)[0]
-        for file in os.listdir(video_path)
-        if os.path.isfile(os.path.join(video_path, file))
-        and os.path.splitext(file)[1].lower() in ('.mkv', '.mp4')
-    ]
+    video_files = {}
+    for file in os.listdir(video_path):
+        file_path = os.path.join(video_path, file)
+        if os.path.isfile(file_path) and os.path.splitext(file)[1].lower() in ('.mkv', '.mp4'):
+            video_files[os.path.splitext(file)[0]] = file
 
     files_directory = os.path.join(os.getcwd(), 'Files')
     os.makedirs(files_directory, exist_ok=True)
+    print(f"Files Directory: {files_directory}")
 
-    with zipfile.ZipFile(subtitle_zip_file, 'r') as local_obj:
-        local_obj.extractall(files_directory)
+    try:
+        with zipfile.ZipFile(subtitle_zip_file, 'r') as local_obj:
+            local_obj.extractall(files_directory)
+        print("Subtitle files extracted successfully.")
+    except Exception as e:
+        print(f"Subtitle Extraction Error: {str(e)}")
 
-    for i, file in enumerate(os.listdir(files_directory)):
-        new_file_name = f'{video_files[i]}.srt'
-        old_file_path = os.path.join(files_directory, file)
-        new_file_path = os.path.join(video_path, new_file_name)
-        shutil.move(old_file_path, new_file_path)
+    subtitle_files = {}
+    for file_name in os.listdir(files_directory):
+        file_path = os.path.join(files_directory, file_name)
+        if os.path.isfile(file_path) and os.path.splitext(file_name)[1].lower() == '.srt':
+            episode = extract_episode_number(file_name)
+            subtitle_files[episode] = file_name
+
+    print("Subtitle Files:")
+    for episode, subtitle_file in subtitle_files.items():
+        print(f"Episode: {episode} - Subtitle File: {subtitle_file}")
+    print()
+
+    renamed_files = set()
+
+    for video_name, video_file in video_files.items():
+        video_episode = extract_episode_number(video_name)
+        print(f"Video File: {video_file} - Episode: {video_episode}")
+        if video_episode and video_episode in subtitle_files:
+            subtitle_file = subtitle_files[video_episode]
+            new_file_name = f'{video_name}.srt'
+            old_file_path = os.path.join(files_directory, subtitle_file)
+            new_file_path = os.path.join(video_path, new_file_name)
+
+            if not os.path.exists(new_file_path):
+                try:
+                    shutil.move(old_file_path, new_file_path)
+                    renamed_files.add(subtitle_file)
+                    print(f"Integration Successful: Renamed subtitle file {subtitle_file} to {new_file_name}")
+                except Exception as e:
+                    print(f"Integration Error: {str(e)}")
+            else:
+                choice = input(f"Subtitle file '{new_file_name}' already exists. Replace? (Y/N): ").lower()
+                if choice == 'y':
+                    try:
+                        shutil.move(old_file_path, new_file_path)
+                        renamed_files.add(subtitle_file)
+                        print(f"Integration Successful: Renamed subtitle file {subtitle_file} to {new_file_name}")
+                    except Exception as e:
+                        print(f"Integration Error: {str(e)}")
+                else:
+                    print(f"Integration Skipped: Subtitle file {subtitle_file} already exists as {new_file_name}")
 
     shutil.rmtree(files_directory, ignore_errors=True)
 
-    return input("Rename Again (Y/N): ").lower()
+    if not renamed_files:
+        print("Error: No video files matched with any subtitle files.")
+        return
 
+    return renamed_files
 
 def main():
     while True:
         input_data = SubtitleRenameInput.create_from_input()
-        again = rename_machine(input_data)
+        renamed_files = rename_machine(input_data)
+        if not renamed_files:
+            print("Error: No video files matched with any subtitle files.")
+            break
+
+        again = input("Rename Again (Y/N): ").lower()
         if again != 'y':
             print("\nThanks for using!\n")
             break
-
 
 if __name__ == '__main__':
     main()
